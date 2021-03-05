b0c12ed6fbc8646c819a552bdec91f6f10471140
jakwie002 <80104865+jakwie002@users.noreply.github.com>
2021-03-05 15:02:24 +0100

    Add files via upload

---
+++ p1/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        //Properties p = System.getProperties();
+        //p.list(System.out);
+        System.getProperty("java.version");
+    }
+
+}
